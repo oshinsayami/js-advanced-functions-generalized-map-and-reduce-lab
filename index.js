17a1b35668b0f4c5b2f00084868b32187385389c
Simran <mdrsimran@gmail.com>
2021-02-09 17:30:56 -0600

    commit tree 1eb8cb1dcf75543225b10d9fcfd7a57247fc6724

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, fxn){
+    let a=[]
+
+    for(let i=0; i< src.length; i++){
+        let b = src[i]
+        a.push(fxn(b))
+    }
+    return a;
+}
+
+function reduce (src, fxn, starting){
+    let r = (!!starting)? starting : src[0]
+
+    let i = (!!starting)? 0 :1
+    for(; i<src.length; i++){
+        r= fxn(src[i],r);
+    }
+    return r;
+}
